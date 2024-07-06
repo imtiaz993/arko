ca75c9d16208021820e6d841c9e8e7d9222ca960
Imtiaz Hussain <imtiazh993@gmail.com>
2024-07-06 20:31:52 +0500

    fixed ui issues

--- app/(Pages)/privacy-policy/page.js
+++ app/(Pages)/privacy-policy/page.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "../../../public/css/style.css";
 
 export const metadata = {
   title: "ARKCO | Privacy Policy",
@@ -6,8 +7,14 @@ export const metadata = {
 
 const PrivacyPolicy = () => {
   return (
-    <div>
-      <h1>Privacy Policy</h1>
+    <div
+      style={{
+        color: "white",
+        padding: "5%",
+        fontFamily: "Century Gothic, sans-serif !important",
+      }}
+    >
+      <h1 style={{marginTop: 0}}>Privacy Policy</h1>
       <p>Last updated: January 11, 2024</p>
       <p>
         This Privacy Policy describes Our policies and procedures on the
@@ -293,10 +300,11 @@ const PrivacyPolicy = () => {
           <p>
             <strong>To contact You:</strong> To contact You by email, telephone
             calls, SMS, or other equivalent forms of electronic communication,
-            such as a mobile application&apos;s push notifications regarding updates
-            or informative communications related to the functionalities,
-            products or contracted services, including the security updates,
-            when necessary or reasonable for their implementation.
+            such as a mobile application&apos;s push notifications regarding
+            updates or informative communications related to the
+            functionalities, products or contracted services, including the
+            security updates, when necessary or reasonable for their
+            implementation.
           </p>
         </li>
         <li>
@@ -389,12 +397,13 @@ const PrivacyPolicy = () => {
       </p>
       <h3>Transfer of Your Personal Data</h3>
       <p>
-        Your information, including Personal Data, is processed at the Company&apos;s
-        operating offices and in any other places where the parties involved in
-        the processing are located. It means that this information may be
-        transferred to — and maintained on — computers located outside of Your
-        state, province, country or other governmental jurisdiction where the
-        data protection laws may differ than those from Your jurisdiction.
+        Your information, including Personal Data, is processed at the
+        Company&apos;s operating offices and in any other places where the
+        parties involved in the processing are located. It means that this
+        information may be transferred to — and maintained on — computers
+        located outside of Your state, province, country or other governmental
+        jurisdiction where the data protection laws may differ than those from
+        Your jurisdiction.
       </p>
       <p>
         Your consent to this Privacy Policy followed by Your submission of such
@@ -486,8 +495,8 @@ const PrivacyPolicy = () => {
       <p>
         Our Service may contain links to other websites that are not operated by
         Us. If You click on a third party link, You will be directed to that
-        third party&apos;s site. We strongly advise You to review the Privacy Policy
-        of every site You visit.
+        third party&apos;s site. We strongly advise You to review the Privacy
+        Policy of every site You visit.
       </p>
       <p>
         We have no control over and assume no responsibility for the content,
